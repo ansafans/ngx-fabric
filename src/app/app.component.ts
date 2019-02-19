44a7d349aead1349edb8876a2428437084eeb729
ansafans <ansafsah@gmail.com>
2019-02-19 21:42:36 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CanvasService } from 'projects/ng-fabricjs/src/lib/Canvas/canvas.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-fabricjs-module';
+
+  public canvasEl;
+  constructor(private canvas:CanvasService){}
+
+  ngOnInit(): void {
+    this.canvasEl=this.canvas.init('canvas'); 
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.canvas.setHeight(500);
+    console.log(this.canvas.getHeight());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgFabricjsModule } from 'projects/ng-fabricjs/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgFabricjsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
